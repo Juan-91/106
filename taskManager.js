e423d8dd7f2a8cf71160259f7cd9f7dcee62d2ff
Juan-91 <Juan.Montiel@sdgku.edu>
2024-08-26 12:37:55 +0930

    #3

--- taskManager.js
+++ taskManager.js
@@ -1,13 +1,13 @@
 function saveTask(){
-    console.log("Task Manager...")
-    const title=$("#txtTitle").val()
+    console.log("Task Manager...");
+    const title=$("#txtTitle").val();
     const description=$("#txtDescription").val();
     const color=$("#txtColor").val();
     const date=$("#txtDate").val();
     const status=$("#selStatus").val();
     const budget=$("#numBudget").val();
 
-    console.log(title, description, color, date, status,budget);
+    //console.log(title, description, color, date, status,budget);
 
     let taskSave=new Task(title, description, color, date, status, budget);
     console.log(taskSave);
@@ -17,17 +17,41 @@ function saveTask(){
         url: "http://fsdiapi.azurewebsites.net/api/tasks/",
         data: JSON.stringify(taskSave),
         contentType: "application/json",
-        success: function (response) {
-          console.log(response)
+        success: function (response){
+          console.log(response);
         },
         error: function (error) {
-          console.log(error)
+          console.log(error);
         }
       });
+      displayTask(taskSave);
+    }
+
+
+    
+    function displayTask(task){
+        let syntax=`
+        <div class="task-container" style="border-color:${task.color}"> 
+            <div class="task">
+                <div class="info">
+                    <h5>${task.title}</h5>
+                    <p>${task.description}</p>
+        </div>
+        <div class="status">${task.status}</div>
+        <div class="date-budget">
+            <span>${task.date}</span>
+            <span>${task.budget}</span>
+                </div>
+            </div>
+        </div>
+        `;
+
+        $("#list").append(syntax);
 }
 
+      
 function init(){
-    $("#btnSave").click(saveTask)
+    $("#btnSave").click(saveTask);
 }
 
 window.onload=init
